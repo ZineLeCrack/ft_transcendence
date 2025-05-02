c606c3ae79983a37b9044976c59c5cfcd758e37a
Nnelo <broudicelouann@gmail.com>
2025-05-02 14:03:50 +0200

    save

--- frontend/src/chat/chat.js
+++ frontend/src/chat/chat.js
@@ -6,10 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
         var text = input.value;
         if (text === "")
             return;
+        var messageWrapper = document.createElement("div");
+        messageWrapper.className = "flex flex-col items-start space-y-1";
+        var username = document.createElement("div");
+        username.className = "text-sm font-semibold text-gray-500 ";
+        username.textContent = "rlebaill";
         var msg = document.createElement("div");
-        msg.className = " bg-purple-500 text-white px-4 py-2 rounded-3xl w-fit max-w-[80%] break-words whitespace-pre-wrap shadow-md shadow-black/50	";
+        msg.className = "bg-purple-500 text-md text-white px-4 py-2 rounded-3xl w-fit max-w-[80%] break-words whitespace-pre-wrap shadow-md shadow-black/50";
         msg.textContent = text;
-        messageBox.appendChild(msg);
+        messageWrapper.appendChild(username);
+        messageWrapper.appendChild(msg);
+        messageBox.appendChild(messageWrapper);
         input.value = "";
         messageBox.scrollTop = messageBox.scrollHeight;
     }
--- frontend/src/chat/chat.ts
+++ frontend/src/chat/chat.ts
@@ -4,20 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
 	const messageBox = document.getElementById("chat-messages") as HTMLDivElement;
   
 	function sendMessage() {
-	  const text = input.value;
-	  if (text === "") return;  
-	  const msg = document.createElement("div");
-  
-	  msg.className = " bg-purple-500 text-white px-4 py-2 rounded-3xl w-fit max-w-[80%] break-words whitespace-pre-wrap shadow-md shadow-black/50	";
-  
-	  msg.textContent = text;
-  
-	  messageBox.appendChild(msg);
-	  input.value = "";
-	  messageBox.scrollTop = messageBox.scrollHeight;
+		const text = input.value;
+		if (text === "") return;
+
+		const messageWrapper = document.createElement("div");
+		messageWrapper.className = "flex flex-col items-start space-y-1";
+
+		const username = document.createElement("div");
+		username.className = "text-sm font-semibold text-gray-500 ";
+		username.textContent = "rlebaill";
+		
+		const msg = document.createElement("div");
+
+		msg.className = "bg-purple-500 text-md text-white px-4 py-2 rounded-3xl w-fit max-w-[80%] break-words whitespace-pre-wrap shadow-md shadow-black/50";
+
+		msg.textContent = text;
+
+		messageWrapper.appendChild(username);
+		messageWrapper.appendChild(msg);
+		messageBox.appendChild(messageWrapper);
+
+		input.value = "";
+		messageBox.scrollTop = messageBox.scrollHeight;
 	}
+
 	sendBtn.addEventListener("click", sendMessage);
 	input.addEventListener("keydown", (e) => {
-	  if (e.key === "Enter") sendMessage();
+		if (e.key === "Enter") sendMessage();
 	});
-  });
+});
