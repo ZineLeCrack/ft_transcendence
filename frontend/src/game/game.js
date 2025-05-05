e502754f77e8e94b6242d52ab7f1025c179d837e
ZineLeCrack <rlebaill27@gmail.com>
2025-05-05 07:48:51 +0200

    chemin reparer

--- frontend/src/game/game.js
+++ frontend/src/game/game.js
@@ -1,31 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var leftBtn = document.getElementById("left-button-game");
-    var rightBtn = document.getElementById("right-button-game");
-    var playBtn = document.getElementById("game-play-button");
-    var gameModeDiv = document.getElementById("game-mode");
-    var modes = ["LOCAL", "MULTI", "AI", "TOURNAMENTS"];
-    var currentIndex = 0;
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const leftBtn = document.getElementById("left-button-game");
+    const rightBtn = document.getElementById("right-button-game");
+    const playBtn = document.getElementById("game-play-button");
+    const gameModeDiv = document.getElementById("game-mode");
+    const modes = ["LOCAL", "MULTI", "AI", "TOURNAMENTS"];
+    let currentIndex = 0;
     function updateDisplay() {
-        var mode = modes[currentIndex];
+        const mode = modes[currentIndex];
         gameModeDiv.textContent = mode;
         if (mode === "LOCAL") {
-            playBtn.onclick = function () { return window.location.href = "src/game/pong.html"; };
+            playBtn.onclick = () => window.location.href = "src/game/pong.html";
         }
         else if (mode === "MULTI") {
-            playBtn.onclick = function () { return window.location.href = "src/game/multiplayer.html"; };
+            playBtn.onclick = () => window.location.href = "src/game/multiplayer.html";
         }
         else if (mode === "AI") {
-            playBtn.onclick = function () { return window.location.href = "src/game/AI.html"; };
+            playBtn.onclick = () => window.location.href = "src/game/AI.html";
         }
         else if (mode === "TOURNAMENTS") {
-            playBtn.onclick = function () { return window.location.href = "src/game/tournaments.html"; };
+            playBtn.onclick = () => window.location.href = "src/game/tournaments.html";
         }
     }
-    rightBtn.addEventListener("click", function () {
+    rightBtn.addEventListener("click", () => {
         currentIndex = (currentIndex + 1) % modes.length;
         updateDisplay();
     });
-    leftBtn.addEventListener("click", function () {
+    leftBtn.addEventListener("click", () => {
         currentIndex = (currentIndex - 1 + modes.length) % modes.length;
         updateDisplay();
     });
