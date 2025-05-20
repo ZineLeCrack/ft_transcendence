800bc716be390cd0d362e875a4f456240f75c91b
Nnelo <broudicelouann@gmail.com>
2025-05-20 11:11:53 +0200

    add date and name for history

--- frontend/src/profile/stats.js
+++ frontend/src/profile/stats.js
@@ -23,16 +23,29 @@ const cardsHistory = [
     {
         imageplayer1: "/frontend/src//images/pdp_cle-berr.png",
         imageplayer2: "/frontend/src/images/pdp_rlebaill.jpeg",
+        usernameplayer2: "rlebaill",
         pointplayer1: 5,
         pointplayer2: 3,
+        date: "20/05/25 at 9:58",
         result: 'win',
     },
     {
         imageplayer1: "/frontend/src/images/pdp_cle-berr.png",
         imageplayer2: "/frontend/src/images/pdp_rlebaill.jpeg",
+        usernameplayer2: "rlebaill",
         pointplayer1: 2,
         pointplayer2: 5,
+        date: "20/05/25 at 10:00",
         result: 'loose',
+    },
+    {
+        imageplayer1: "/frontend/src/images/pdp_cle-berr.png",
+        imageplayer2: "/frontend/src/images/pdp_rlebaill.jpeg",
+        usernameplayer2: "rlebaill",
+        pointplayer1: 5,
+        pointplayer2: 0,
+        date: "20/03/25 at 10:00",
+        result: 'win',
     },
 ];
 function generateCards(cardsHistory) {
@@ -41,17 +54,23 @@ function generateCards(cardsHistory) {
         cardsHistory.forEach(CardHistory => {
             const cardElement = document.createElement('div');
             if (CardHistory.result === 'loose') {
-                cardElement.className = 'bg-red-600/30 border-4 border-red-500 shadow-[0_0_10px_#ff0000,0_0_20px_#ff0000,0_0_40px_#ff0000]  w-4/5 h-[195px] mx-auto flex items-center justify-start rounded-xl';
+                cardElement.className = 'bg-red-600/30 border-4 border-red-500 shadow-[0_0_10px_#ff0000,0_0_20px_#ff0000,0_0_40px_#ff0000] w-4/5 h-[195px] mx-auto flex items-center justify-start rounded-xl';
             }
             else {
                 cardElement.className = 'bg-[#00ff88]/30 border-4 border-[#00ff88] shadow-[0_0_10px_#00ff88,0_0_20px_#00ff88,0_0_40px_#00ff88] w-4/5 h-[195px] mx-auto flex items-center justify-start rounded-xl';
             }
             cardElement.innerHTML = `
-					<img src="${CardHistory.imageplayer1}" class="rounded-full ml-28 w-[170px] h-[170px] "alt="">
+					<div class=" ml-28 flex flex-col items-center">
+      					<img src="${CardHistory.imageplayer1}" class="rounded-full w-[140px] h-[150px]" alt="">
+     					<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#00FFFF] mt-1">You</div>
+    				</div>
 					<div class="text-center text-8xl ml-28 font-bold text-[#00FFFF] drop-shadow-[0_0_10px_#00FFFF]">${CardHistory.pointplayer1}</div>
 					<img src="/frontend/src/images/VS.png" class="rounded-full ml-14 w-[270px] h-[270px] "alt="">
 					<div class="text-center text-8xl ml-14 font-bold text-[#FF007A] drop-shadow-[0_0_10px_#FF007A]">${CardHistory.pointplayer2}</div>
-					<img src="${CardHistory.imageplayer2}" class="rounded-full ml-28 w-[170px] h-[170px] "alt="">
+					<div class=" ml-28 flex flex-col items-center">
+      					<img src="${CardHistory.imageplayer2}" class="rounded-full w-[140px] h-[150px]" alt="">
+     					<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#FF007A] mt-1">${CardHistory.usernameplayer2}</div>
+    				</div>
 				</div>
 			`;
             container.appendChild(cardElement);
--- frontend/src/profile/stats.ts
+++ frontend/src/profile/stats.ts
@@ -35,8 +35,10 @@ tournamentBtn.addEventListener('click', () => {
 interface CardHistory {
 	imageplayer1: string;
 	imageplayer2: string;
+	usernameplayer2: string;
 	pointplayer1: number;
 	pointplayer2: number;
+	date: string;
 	result: 'win' | 'loose';
 }
 
@@ -44,19 +46,33 @@ const cardsHistory: CardHistory[] = [
   {
 	imageplayer1: "/frontend/src//images/pdp_cle-berr.png",
 	imageplayer2: "/frontend/src/images/pdp_rlebaill.jpeg",
+	usernameplayer2: "rlebaill",
 	pointplayer1: 5,
 	pointplayer2: 3,
+	date: "20/05/25 at 9:58",
 	result: 'win',
   },
 
   {
 	imageplayer1: "/frontend/src/images/pdp_cle-berr.png",
 	imageplayer2: "/frontend/src/images/pdp_rlebaill.jpeg",
+	usernameplayer2: "rlebaill",
 	pointplayer1: 2,
 	pointplayer2: 5,
+	date: "20/05/25 at 10:00",
 	result: 'loose',
   },
-  
+  {
+	imageplayer1: "/frontend/src/images/pdp_cle-berr.png",
+	imageplayer2: "/frontend/src/images/pdp_rlebaill.jpeg",
+	usernameplayer2: "rlebaill",
+	pointplayer1: 5,
+	pointplayer2: 0,
+	date: "20/03/25 at 10:00",
+	result: 'win',
+  },
+
+
 ];
 
 function generateCards(cardsHistory: CardHistory[]): void 
@@ -68,7 +84,7 @@ function generateCards(cardsHistory: CardHistory[]): void
 			const cardElement = document.createElement('div');
 			if (CardHistory.result === 'loose')
 			{
-				cardElement.className = 'bg-red-600/30 border-4 border-red-500 shadow-[0_0_10px_#ff0000,0_0_20px_#ff0000,0_0_40px_#ff0000]  w-4/5 h-[195px] mx-auto flex items-center justify-start rounded-xl';
+				cardElement.className = 'bg-red-600/30 border-4 border-red-500 shadow-[0_0_10px_#ff0000,0_0_20px_#ff0000,0_0_40px_#ff0000] w-4/5 h-[195px] mx-auto flex items-center justify-start rounded-xl';
 			}
 			else
 			{
@@ -76,11 +92,17 @@ function generateCards(cardsHistory: CardHistory[]): void
 			}
 	  
 			cardElement.innerHTML = `
-					<img src="${CardHistory.imageplayer1}" class="rounded-full ml-28 w-[170px] h-[170px] "alt="">
+					<div class=" ml-28 flex flex-col items-center">
+      					<img src="${CardHistory.imageplayer1}" class="rounded-full w-[140px] h-[150px]" alt="">
+     					<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#00FFFF] mt-1">You</div>
+    				</div>
 					<div class="text-center text-8xl ml-28 font-bold text-[#00FFFF] drop-shadow-[0_0_10px_#00FFFF]">${CardHistory.pointplayer1}</div>
 					<img src="/frontend/src/images/VS.png" class="rounded-full ml-14 w-[270px] h-[270px] "alt="">
 					<div class="text-center text-8xl ml-14 font-bold text-[#FF007A] drop-shadow-[0_0_10px_#FF007A]">${CardHistory.pointplayer2}</div>
-					<img src="${CardHistory.imageplayer2}" class="rounded-full ml-28 w-[170px] h-[170px] "alt="">
+					<div class=" ml-28 flex flex-col items-center">
+      					<img src="${CardHistory.imageplayer2}" class="rounded-full w-[140px] h-[150px]" alt="">
+     					<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#FF007A] mt-1">${CardHistory.usernameplayer2}</div>
+    				</div>
 				</div>
 			`;
 
