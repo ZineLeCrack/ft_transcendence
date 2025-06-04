1cfb1a32950da307aeef9f7d490c665058381bf9
ZineLeCrack <rlebaillif27@gmail.com>
2025-06-04 14:47:16 +0200

    websocket chat

--- database/chat/websocket_chat.ts
+++ database/chat/websocket_chat.ts
@@ -22,30 +22,32 @@ export function setupWebSocket(server: any) {
 		ws.on('message', async (message) => {
 			try {
 				const data = JSON.parse(message.toString());
-				const { token, content } = data; 
+				const { type, token, content } = data;
+				
+				if (type === 'new_message') {
+					if (!token || !content) return;
+					let id_user;
+					try {
+						const decoded = jwt.verify(token, JWT_SECRET);
+						id_user = (decoded as { userId: string }).userId;
+					}
+					catch (err) {
+						console.log(err);
+						return;
+					}
+					const dbusers = await getDb_user();
+					const db = await getDb_chat();
+					const response = await dbusers.get(`SELECT name FROM users WHERE id = ?`,[id_user]);
+					const username = response.name;
+					await db.run(
+						`INSERT INTO chat (username, content) VALUES (?, ?)`,
+						[username, content]
+					);
 
-				if (!token || !content) return;
-				let id_user;
-				try {
-					const decoded = jwt.verify(token, JWT_SECRET);
-					id_user = (decoded as { userId: string }).userId;
-				}
-				catch (err) {
-					console.log(err);
-					return;
-				}
-				const dbusers = await getDb_user();
-				const db = await getDb_chat();
-				const response = await dbusers.get(`SELECT name FROM users WHERE id = ?`,[id_user]);
-				const username = response.name;
-				await db.run(
-					`INSERT INTO chat (username, content) VALUES (?, ?)`,
-					[username, content]
-				);
-
-				for (const client of clients) {
-					if (client.readyState === ws.OPEN) {
-						client.send(JSON.stringify({ username, content }));
+					for (const client of clients) {
+						if (client.readyState === ws.OPEN) {
+							client.send(JSON.stringify({ type, username, content }));
+						}
 					}
 				}
 			} catch (err) {
