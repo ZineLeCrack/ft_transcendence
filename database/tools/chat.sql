31f9ae70221b74afa7c28268d276b712051a5386
Nnelo <broudicelouann@gmail.com>
2025-06-07 21:42:18 +0200

    merge cyp

--- database/tools/chat.sql
+++ database/tools/chat.sql
@@ -13,7 +13,7 @@ CREATE TABLE privatechat (
     username1 TEXT NOT NULL,
     username2 TEXT NOT NULL,
     content TEXT NOT NULL,
-    pongRequest INTEGER DEFAULT 0,
+    pongRequest INTEGER DEFAULT 0, -- 0 no resquest, 1 request send or received, 2 request accept, 3 request decline
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 
 );
