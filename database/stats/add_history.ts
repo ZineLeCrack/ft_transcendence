0429bf8161fce44d7b1afede84944317c80c25aa
Nnelo <broudicelouann@gmail.com>
2025-06-10 12:47:02 +0200

    stats tournament (pas fini)

--- database/stats/add_history.ts
+++ database/stats/add_history.ts
@@ -3,17 +3,26 @@ import { getDb_user } from '../database.js';
 
 export default async function addhistoryRoutes(fastify: FastifyInstance) {
 	fastify.post('/addhistory', async (request, reply) => {
-		const { Id1, Id2, score1, score2 } = request.body as { Id1: string, Id2: string, score1: number, score2: number };
+		const { Id1, Id2, score1, score2, tournament } = request.body as { Id1: string, Id2: string, score1: number, score2: number, tournament: boolean };
 
 
 		try {
 
+			let tournament_bool;
+			if (!tournament)
+			{
+				tournament_bool = 0;
+			}
+			else 
+			{
+				tournament_bool = 1;
+			}
 			const db = await getDb_user();
 			await db.run(
 				`
-				INSERT INTO history (id_player1, id_player2, point_player1, point_player2) values (?, ?, ?, ?);
+				INSERT INTO history (id_player1, id_player2, point_player1, point_player2, tournament) values (?, ?, ?, ?, ?);
 				`,
-				[Id1, Id2, score1, score2]
+				[Id1, Id2, score1, score2 , tournament_bool]
 			);
 
 			await db.run(
