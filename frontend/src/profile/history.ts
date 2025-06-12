7eeed7a94e29a0a90e55c2c54120ba9c734248b4
lelanglo <l20langlois@gmail.com>
2025-06-12 13:52:29 +0200

    problem fixed in history

--- frontend/src/profile/history.ts
+++ frontend/src/profile/history.ts
@@ -33,6 +33,7 @@ export function generateCardsHistory(div: string ,cardsHistory: CardHistory[], u
 			const cardElement = document.createElement('div');
 			if (username === CardHistory.usernameplayer1)
 			{
+				const id = Math.random() * 1000000000;
 				if (CardHistory.pointplayer1 > CardHistory.pointplayer2)
 				{
 					cardElement.className = 'relative w-4/5 h-[195px] mx-auto grid grid-cols-5 items-center rounded-xl border-4 p-4 bg-[#00ff88]/30 border-[#00ff88] shadow-[0_0_10px_#00ff88,0_0_20px_#00ff88,0_0_40px_#00ff88]';
@@ -44,7 +45,7 @@ export function generateCardsHistory(div: string ,cardsHistory: CardHistory[], u
 
 				cardElement.innerHTML = `
 					<div class="flex flex-col items-center">
-						<div id="profile-pic-player1-${CardHistory.usernameplayer1}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
+						<div id="profile-pic-player1-${id}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
 						<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#00FFFF] mt-1">${CardHistory.usernameplayer1}</div>
 					</div>
 
@@ -55,18 +56,19 @@ export function generateCardsHistory(div: string ,cardsHistory: CardHistory[], u
 					<div class="text-8xl font-bold text-[#FF007A] drop-shadow-[0_0_10px_#FF007A] text-center">${CardHistory.pointplayer2}</div>
 
 					<div class="flex flex-col items-center">
-						<div id="profile-pic-player2-${CardHistory.usernameplayer2}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
+						<div id="profile-pic-player2-${id}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
 						<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#FF007A] mt-1">${CardHistory.usernameplayer2}</div>
 					</div>
 
 					<div class="absolute bottom-2 left-1/2 -translate-x-1/2 text-sm text-white font-semibold drop-shadow-[0_0_5px_#00FFFF]">${CardHistory.date}</div>
 				`;
 
-				loadProfilePicture(`profile-pic-player1-${CardHistory.usernameplayer1}`, CardHistory.usernameplayer1);
-				loadProfilePicture(`profile-pic-player2-${CardHistory.usernameplayer2}`, CardHistory.usernameplayer2);
+				loadProfilePicture(`profile-pic-player1-${id}`, CardHistory.usernameplayer1);
+				loadProfilePicture(`profile-pic-player2-${id}`, CardHistory.usernameplayer2);
 			}
 			else
 			{
+				const id = Math.random() * 1000000000;
 				if (CardHistory.pointplayer1 < CardHistory.pointplayer2)
 				{
 					cardElement.className = 'relative w-4/5 h-[195px] mx-auto grid grid-cols-5 items-center rounded-xl border-4 p-4 bg-[#00ff88]/30 border-[#00ff88] shadow-[0_0_10px_#00ff88,0_0_20px_#00ff88,0_0_40px_#00ff88]';
@@ -77,7 +79,7 @@ export function generateCardsHistory(div: string ,cardsHistory: CardHistory[], u
 				}
 				cardElement.innerHTML =`
 					<div class="flex flex-col items-center">
-						<div id="profile-lose1-${CardHistory.usernameplayer1}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
+						<div id="profile-lose1-${id}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
 						<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#00FFFF] mt-1">${CardHistory.usernameplayer2}</div>
 					</div>
 					
@@ -87,15 +89,15 @@ export function generateCardsHistory(div: string ,cardsHistory: CardHistory[], u
 					<div class="text-8xl font-bold text-[#FF007A] drop-shadow-[0_0_10px_#FF007A] text-center">${CardHistory.pointplayer1}</div>
 					
 					<div class="flex flex-col items-center">
-						<div id="profile-lose2-${CardHistory.usernameplayer2}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
+						<div id="profile-lose2-${id}" class="w-[120px] h-[120px] overflow-hidden rounded-full"></div>
 						<div class="text-lg font-bold text-white drop-shadow-[0_0_10px_#FF007A] mt-1">${CardHistory.usernameplayer1}</div>
 					</div>
 					
 					<div class="absolute bottom-2 left-1/2 -translate-x-1/2 text-sm text-white font-semibold drop-shadow-[0_0_5px_#00FFFF]">${CardHistory.date}</div>
 				`;
 
-				loadProfilePicture(`profile-lose1-${CardHistory.usernameplayer2}`, CardHistory.usernameplayer2);
-				loadProfilePicture(`profile-lose2-${CardHistory.usernameplayer1}`, CardHistory.usernameplayer1);
+				loadProfilePicture(`profile-lose1-${id}`, CardHistory.usernameplayer2);
+				loadProfilePicture(`profile-lose2-${id}`, CardHistory.usernameplayer1);
 			}
 			container.appendChild(cardElement);
 		});
