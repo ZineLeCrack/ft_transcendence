573f7ce14a2e535c4001ffb7db8df42d92b3a408
Nnelo <broudicelouann@gmail.com>
2025-06-16 13:07:44 +0200

    bouton pong qui reaparait plus quand on click vite

--- frontend/src/chat/friendchat.ts
+++ frontend/src/chat/friendchat.ts
@@ -97,17 +97,9 @@ export default async function initFriendChat() {
 			friendslist.innerHTML = `<p class="text-[#FF2E9F]">${noFriendsText}</p>`;
 			return ;
 		}
-		Friend.forEach(async Friend => {
+		Friend.forEach(Friend => {
 			const friendElement = document.createElement('div');
 			friendElement.className = 'flex gap-8';
-			const tokenID = sessionStorage.getItem("token");
-
-			const blockCheck = await fetch("/api/isblock", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ tokenID, target: Friend.username })
-			});
-			const block = await blockCheck.json();
 
 			if (Friend.status === 1) {
 				friendElement.innerHTML = `
@@ -125,7 +117,7 @@ export default async function initFriendChat() {
 						</a>
 					</div>`;
 					loadProfilePicture(`profile-pic-friend-online-${Friend.username}`, Friend.username);
-			} else if (block.status === 0) { 
+			} else { 
 				friendElement.innerHTML = `<div class="flex-shrink-0 h-[72px] w-20 flex flex-col items-center">
 						<div class="relative">
 							<button id="Friend-button-${Friend.username}" class="group w-12 h-12 rounded-full border-2 border-[#FF2E9F] hover:shadow-[0_0_10px_#FF2E9F] transition-shadow">
@@ -166,14 +158,8 @@ export default async function initFriendChat() {
 
 	friendButtons.forEach(button => {
 		button.addEventListener('click', async () => {
-			const oldPongBtn = document.getElementById('pong-send');
-			if (oldPongBtn)
-				oldPongBtn.remove();
 			const username = button.id.split('-').pop();
-			
-			const existingChats = document.querySelectorAll('[id^="chat-messages-"]');
-			existingChats.forEach(chat => chat.remove());
-			
+
 			const tokenID = sessionStorage.getItem("token");
 			const target = username;
 			const friendCheck = await fetch("/api/isfriend", {
@@ -197,15 +183,19 @@ export default async function initFriendChat() {
 				return ;
 			}
 
+			const existingChatArea = document.getElementById(`chat-messages-${username}`);
+			if (existingChatArea)
+				existingChatArea.remove();
+
 			const chatArea = document.createElement('div');
 			chatArea.id = `chat-messages-${username}`;
 			chatArea.className = 'flex-1 flex flex-col space-y-4';
 			chatContainers.appendChild(chatArea);
-			const chatTranslate = translate('chat')
+			const chatTranslate = translate('chat');
 			chatInfo.innerHTML = `
-			<span class="mr-2 text-[#FF2E9F]">⚡</span>
-			${chatTranslate}://${username}
-			<span class="ml-2 animate-pulse text-[#FF2E9F]">_</span>`;
+				<span class="mr-2 text-[#FF2E9F]">⚡</span>
+				${chatTranslate}://${username}
+				<span class="ml-2 animate-pulse text-[#FF2E9F]">_</span>`;
 
 			const checkUser = await fetch(`/api/verifuser`, {
 				method: 'POST',
@@ -258,6 +248,9 @@ export default async function initFriendChat() {
 			}
 
 			const inputArea = document.getElementById('input-Area') as HTMLDivElement;
+			const oldPongBtn = document.getElementById('pong-send');
+			if (oldPongBtn)
+				oldPongBtn.remove();
 			const pongBtn = document.createElement('div');
 			pongBtn.id = 'pong-send';
 			pongBtn.className = 'w-8 h-8 rounded flex items-center justify-center hover:bg-[#00FFFF]/10';
