008ffb89d39985b18da4bca2d93d4668123ab1e7
lelanglo <l20langlois@gmail.com>
2025-06-17 12:45:31 +0200

    ca marche pas donc on magouille et ca marche

--- frontend/src/success.ts
+++ frontend/src/success.ts
@@ -4,7 +4,7 @@ export default function initSuccess(content?: string)
 	const successPopUpHTML = `<div id="success-popup" class="fixed left-1/2 -translate-x-1/2 w-full max-w-md p-4 z-50 font-mono">
     <div class="bg-black/70 backdrop-blur-lg border-2 border-[#FFD700] rounded-xl shadow-[0_0_15px_#FFD700]">
         <div class="flex items-center justify-between px-6 py-3 border-b border-[#FFD700]/50">
-            <h3 class="text-xl font-bold text-[#00FFFF]" data-i18n="Success_trad" >✅</h3>
+            <h3 class="text-xl font-bold text-[#00FFFF]" >✅</h3>
             <button id="close-success-popup" class="text-[#FFD700] hover:text-white text-3xl font-bold leading-none hover:opacity-75 transition-opacity duration-200">&times;</button>
         </div>
         <div class="p-6">
