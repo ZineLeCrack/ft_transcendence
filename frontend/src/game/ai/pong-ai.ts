b76e580aa77bfdb5bac3c1c8489a4778bd5e8920
lelanglo <l20langlois@gmail.com>
2025-06-17 15:50:34 +0200

    nom traduis

--- frontend/src/game/ai/pong-ai.ts
+++ frontend/src/game/ai/pong-ai.ts
@@ -75,9 +75,14 @@ export default async function initPong() {
 		game.shadowBlur = 10;
 
 		let new_message;
-		if (message === "to_start" || message === "1_win") {
+		if (message === "to_start") {
 			new_message = translate(message);
-		} else if (message === "2_win") {
+		}
+		else if (message === "1_win")
+		{
+			new_message = `${info.original} ${translate("1_win")}`;
+		} 
+		else if (message === "2_win") {
 			new_message = translate("ai_win");
 		} else {
 			new_message = message;
